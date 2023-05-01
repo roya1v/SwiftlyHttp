76250f3b6ddfe0b88dbd3da2d3745c4df2e6cec4
Mike <m.shevelinski@gmail.com>
2023-05-01 15:34:29 +0200

    Rename to correct case

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftly-http",
+    name: "SwiftlyHttp",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swiftly-http",
-            targets: ["swiftly-http"]),
+            name: "SwiftlyHttp",
+            targets: ["SwiftlyHttp"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swiftly-http"),
+            name: "SwiftlyHttp"),
         .testTarget(
-            name: "swiftly-httpTests",
-            dependencies: ["swiftly-http"]),
+            name: "SwiftlyHttpTests",
+            dependencies: ["SwiftlyHttp"]),
     ]
 )
