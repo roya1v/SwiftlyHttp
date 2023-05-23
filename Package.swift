94fd93b432016d7b91180f417094654462b18bdf
Mike <m.shevelinski@gmail.com>
2023-05-23 19:57:45 +0200

    Add plugin for docs

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(
             name: "SwiftlyHttp",
             targets: ["SwiftlyHttp"]),
-    ],
+    ], dependencies: [.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
